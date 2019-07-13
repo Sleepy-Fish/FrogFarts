04b0caa545a166335a2c58ce9c3ef9c5bc1efbcd
Caleb Rogers <rogersjcaleb@gmail.com>
2019-07-12 19:51:46 -0700

    Pixi added and launched

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hi');
+import * as PIXI from 'pixi.js';
+// Create a Pixi Application
+const app = new PIXI.Application({ width: 256, height: 256 });
+
+// Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
