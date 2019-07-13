45a2202c60869821bab32d3099dc2c27886e4de0
Caleb Rogers <rogersjcaleb@gmail.com>
2019-07-12 19:34:42 -0700

    Add linter and readme update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GameJam
 Jam
+
+# Developing
+
+1. Run `npm install`
+2. Run `npm run develop`
+3. Open browser and navigate to given URL.
+4. Webpack dev server will auto reload modules for you. When in doubt, refresh the page.
+
+# Building
+
+1. Run `npm install`
+2. Run `npm run build`
+3. Serve the contents of `/dist` however you please.
--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,16 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          // eslint options (if necessary)
+          formatter: "codeframe",
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           'css-loader'
